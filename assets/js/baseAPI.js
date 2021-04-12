57b39c51b3b1bd0c4910e11ec872ee83c68d73b1
wpislog <363740649@qq.com>
2021-04-12 19:23:04 +0800

    已经做完了！

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,26 @@
+<<<<<<< Updated upstream
 $.ajaxprefilter(function(option){
     option.url = 'http:/api-breakingnews-web.itheima.net' +options.url
+=======
+$.ajaxprefilter(function (option) {
+    option.url = 'http:/api-breakingnews-web.itheima.net' + options.url
+
+
+    If(options.url.includes('/my/')) {
+        option.headers = {
+            Authorization: localStorage.token || ''
+        }
+        option.complete = function (response) {
+            console.log(response);
+            if (
+                response.responseJSON.status === 1 &&
+                response.responseJSON.message === '身份认证失败！'
+            ) {
+                location.href = '/login.html'
+            }
+        }
+    }
+}
+
+>>>>>>> Stashed changes
 })
